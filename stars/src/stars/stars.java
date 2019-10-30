54f9660ab20b37eda24fa1fc121144b36d2cab6f
Jafa-R <56623173+Jafa-R@users.noreply.github.com>
2019-10-30 13:48:05 +0200

    v2

--- stars/src/stars/stars.java
+++ stars/src/stars/stars.java
@@ -3,8 +3,12 @@ package stars;
 public class stars {
 
 	public static void main(String[] args) {
-		for(int i=0;i<10;i++){
-			System.out.println("*");
+		for(int i=0;i<5;i++){
+			
+			for(int j=0;j<10;j++){
+			System.out.print("*");
+		}
+			System.out.println();
 		}
 
 	}
