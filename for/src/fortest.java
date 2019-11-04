02fe5fca2cddbb9b449374080a55f1d0727b2515
Jafa-R <56623173+Jafa-R@users.noreply.github.com>
2019-11-04 09:51:41 +0200

    no edits

--- for/src/fortest.java
+++ for/src/fortest.java
@@ -4,11 +4,21 @@ import java.util.Scanner;
 public class fortest {
 
 	public static void main(String[] args) {
-		Scanner scannum = new Scanner(System.in);
+		Scanner get = new Scanner(System.in);
 		System.out.println("Enter num1:");
-		int num1 = scannum.nextInt();
+		int num1 = get.nextInt();
+		int i=0;
+		char a='*';
+		int c=a*num1;
+			if(i<num1){
+				for (i=0;i<num1;i++){
+				System.out.println(""+c);
+			
+			}
+				
+		}
 		System.out.println("Enter num2:");
-		int num2 = scannum.nextInt();
+		int num2 = get.nextInt();
 		int sum=num1+num2;
 		System.out.println("sum = "+sum);
 		
