b1cebff5e067560e8e3d1fc3c2f808224c51a1c4
Jafa-R <56623173+Jafa-R@users.noreply.github.com>
2019-11-05 22:19:20 +0200

    v2

--- square/src/square/squaretest.java
+++ square/src/square/squaretest.java
@@ -5,17 +5,19 @@ import java.util.Scanner;
 public class squaretest {
 
 	public static void main(String[] args) {
-		Scanner get= new Scanner(System.in);
-		System.out.print("pleas enter num:");
-		int n=get.nextInt();
-	    int mod=0;
+		Scanner scan= new Scanner(System.in);
+		System.out.print("please enter num:");
+		int n=scan.nextInt();
+		System.out.print("please enter the sympol:");
+		String sympol = scan.next();
+	    int m=0;
 	    
 	    for(int i = 0; i<n*n; i++){
-	           if (mod % n == 0){
+	           if (m % n == 0){
 	    System.out.println("");
 	    	    }
-	           System.out.print("*");
-	           mod++;
+	           System.out.print(sympol);
+	           m++;
 	  }
 	}
 }
